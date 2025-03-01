7cb47fb40d8bca2756b00f8ec88797cb54f922c8
oybek6445 <oybek887700343@gmail.com>
2025-03-01 10:53:57 +0500

    fix-2

--- assets/js/meyawo.js
+++ assets/js/meyawo.js
@@ -12,6 +12,18 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Formning default submit harakatini to'xtatish
+    
+    const message = document.getElementById("message").value;
+    const ism = document.getElementById("ism").value;
+    const email = document.getElementById("email").value;
+
+    console.log("Ism:", ism);
+    console.log("Email:", email);
+    console.log("Xabar:", message);
+});
+
 // smooth scroll
 $(document).ready(function(){
     $(".navbar .nav-link").on('click', function(event) {
